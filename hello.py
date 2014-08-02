46a855f17d227a4d288b8790925c0facd1659c66
Takayuki Nakazato <(nakasan79@gmail.com)>
2014-08-02 15:39:23 +0900

    first

---
+++ hello.py
@@ -0,0 +1,14 @@
+# coding: UTF-8
+# リスト
+sales = [255, 100, 353, 400]
+
+print len(sales) #4
+print sales[2] #353
+
+sales[2] = 100
+print sales[2] #100
+# in
+# print 100 in sales # True
+# range
+print range(3, 10, 2)
+
